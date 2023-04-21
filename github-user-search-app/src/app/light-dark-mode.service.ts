08e4d9eb206159b24f40dc8cb73bdb5e58178d5b
Corentin Bauche <corentin.bauche@gmail.com>
2023-04-21 15:49:48 +0200

    Ajout detection darkmode OS + texte icon dynamique

---
+++ github-user-search-app/src/app/light-dark-mode.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LightDarkModeService {
+
+  constructor() {}
+  
+  
+
+ 
+
+  // 1 - CHECK PREFERENCE UTILISATEUR
+  // 2 - RETURN SI PREFERENCE, SINON LIGHT
+  // 3 - SAVE DANS INDEXDB 
+}
--- github-user-search-app/src/app/light-dark-mode/light-dark-mode.component.ts
+++ github-user-search-app/src/app/light-dark-mode/light-dark-mode.component.ts
@@ -1,13 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LightDarkModeService } from '../light-dark-mode.service';
 
 @Component({
   selector: 'app-light-dark-mode',
   templateUrl: './light-dark-mode.component.html',
   styleUrls: ['./light-dark-mode.component.sass']
 })
-export class LightDarkModeComponent {
-  toggleMode(){
-    const el = document.querySelector('body');
-    el?.classList.toggle('dark');
+export class LightDarkModeComponent implements OnInit{
+  themeToSwitch:string='';
+  iconToSwitch:string=''; 
+  el = document.body;
+
+  constructor(){}
+
+  ngOnInit():void{
+   this.detectPreferColorScheme()
   }
-}
+  
+  detectPreferColorScheme(){
+    let colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+     
+    if (colorSchemeQuery.matches){
+      this.toggleLightDarkMode(true)
+     }
+     else{
+      this.toggleLightDarkMode(false)
+     }
+     colorSchemeQuery.addEventListener('change',e=>{if(e.matches){this.toggleLightDarkMode(true)}else{this.toggleLightDarkMode(false)}});
+   }
+
+   buttonSwitch(){
+    if(!this.el.classList.contains('dark')){
+      this.toggleLightDarkMode(true);
+    }
+    else{
+      this.toggleLightDarkMode(false);
+    }
+   }
+
+  toggleLightDarkMode(theme:boolean){
+    if(theme && !this.el.classList.contains('dark')){
+      this.el.classList.add('dark');
+      this.themeToSwitch='LIGHT';
+      document.getElementById('header__darkmode')?.classList.add('header__darkmode_dark')
+      document.getElementById('header__darkmode')?.classList.remove('header__darkmode_light')
+    }
+    else{
+      this.el.classList.remove('dark');
+      this.themeToSwitch='DARK';
+      document.getElementById('header__darkmode')?.classList.add('header__darkmode_light')
+      document.getElementById('header__darkmode')?.classList.remove('header__darkmode_dark')
+    }
+  }
+    
+  }
